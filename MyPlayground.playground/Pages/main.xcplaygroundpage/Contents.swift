5c81cd9e51da8d63549621ea06baf44e45bbd5b6
ugowe <joe@spheares.com>
2016-07-06 14:07:00 -0400

    add code to Swift FunctionsLab lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,15 +12,22 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
+func helloWorld() {
+    print("Hello, world")
+}
 
-
-
+helloWorld()
 
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
+func myShot() {
+    let sentence = "I am not giving away my shot."
+    print(sentence)
+}
 
+myShot()
 
 
 
@@ -29,24 +36,36 @@
  */
 // write your code here
 
+func sayGoodbye(name: String) {
+    print("Have a nice day, \(name)!")
+}
 
-
+sayGoodbye("Joe")
+sayGoodbye("Lin")
+sayGoodbye("Sarah")
 
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
+var hamilton = "Alexander Hamilton"
+sayGoodbye(hamilton)
 
-
-
-
+hamilton = "Aaron Burr"
+sayGoodbye(hamilton)
 /*: question5
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign it to a new variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
 // write your code here
 
+func sayGoodMorning() {
+    var joe = "Joe Fork"
+    print("Don't talk to me \(joe)! I need my coffee.")
+    joe = "Joseph Smith"
+    print("Don't talk to me \(joe)! I need my coffee.")
+}
 
-
+sayGoodMorning()
 
 /*:
  [Solution](solution)
